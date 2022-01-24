b8e2687c51e451dcad50c3c22bdffbe5c4a9f2ac
DaRkLoRd-5 <42468360+DaRkLoRd-5@users.noreply.github.com>
2022-01-24 15:34:15 +0530

    Add files via upload

---
+++ simple_client.c (edited by jairaj).c
@@ -0,0 +1,182 @@
+#include "cs104_connection.h"
+#include "hal_time.h"
+#include "hal_thread.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+/* Callback handler to log sent or received messages (optional) */
+static void
+rawMessageHandler (void* parameter, uint8_t* msg, int msgSize, bool sent)
+{
+    if (sent)
+        printf("SEND: ");
+    else
+        printf("RCVD: ");
+
+    int i;
+    for (i = 0; i < msgSize; i++) {
+        printf("%02x ", msg[i]);
+    }
+
+    printf("\n");
+}
+
+/* Connection event handler */
+static void
+connectionHandler (void* parameter, CS104_Connection connection, CS104_ConnectionEvent event)
+{
+    switch (event) {
+    case CS104_CONNECTION_OPENED:
+        printf("Connection established\n");
+        break;
+    case CS104_CONNECTION_CLOSED:
+        printf("Connection closed\n");
+        break;
+    case CS104_CONNECTION_STARTDT_CON_RECEIVED:
+        printf("Received STARTDT_CON\n");
+        break;
+    case CS104_CONNECTION_STOPDT_CON_RECEIVED:
+        printf("Received STOPDT_CON\n");
+        break;
+    }
+}
+
+/*
+ * CS101_ASDUReceivedHandler implementation
+ *
+ * For CS104 the address parameter has to be ignored
+ */
+struct DataExchange {
+    int i1;
+    int i2;
+} *pWrite;
+
+asduReceivedHandler (void* parameter, int address, CS101_ASDU asdu)
+{
+
+	int dummy = CS101_ASDU_getTypeID(asdu);
+    printf("RECVD ASDU type: %s(%i) elements: %i\n",
+            TypeID_toString(dummy),
+            CS101_ASDU_getTypeID(asdu),
+            CS101_ASDU_getNumberOfElements(asdu));
+    
+	pWrite->i1=dummy;
+    printf("size: %d\r\n", sizeof(pWrite));
+    if (CS101_ASDU_getTypeID(asdu) == M_ME_TE_1) {
+
+        printf("  measured scaled values with CP56Time2a timestamp:\n");
+
+        int i;
+
+        for (i = 0; i < CS101_ASDU_getNumberOfElements(asdu); i++) {
+
+            MeasuredValueScaledWithCP56Time2a io =
+                    (MeasuredValueScaledWithCP56Time2a) CS101_ASDU_getElement(asdu, i);
+
+            printf("    IOA: %i value: %i\n",
+                    InformationObject_getObjectAddress((InformationObject) io),
+                    MeasuredValueScaled_getValue((MeasuredValueScaled) io)
+            );
+
+            MeasuredValueScaledWithCP56Time2a_destroy(io);
+        }
+    }
+    else if (CS101_ASDU_getTypeID(asdu) == M_SP_NA_1) {
+        printf("  single point information:\n");
+
+        int i;
+
+        for (i = 0; i < CS101_ASDU_getNumberOfElements(asdu); i++) {
+
+            SinglePointInformation io =
+                    (SinglePointInformation) CS101_ASDU_getElement(asdu, i);
+
+            printf("    IOA: %i value: %i\n",
+                    InformationObject_getObjectAddress((InformationObject) io),
+                    SinglePointInformation_getValue((SinglePointInformation) io)
+            );
+
+            SinglePointInformation_destroy(io);
+        }
+    }
+
+    
+    return true;
+}
+
+int
+main(int argc, char** argv)
+{
+
+	char sSharedMemWriteName[100] = "_CODESYS_SharedMemoryTest_Read";                                   /*-------->4 Lines added - Parva*/
+    int fdWrite = shm_open(sSharedMemWriteName, O_CREAT | O_RDWR, S_IRWXU | S_IRWXG);
+    printf("Shared Memory Write: %s %d\n\n",sSharedMemWriteName, fdWrite);
+    ftruncate(fdWrite, sizeof(*pWrite));
+    pWrite = mmap(0, sizeof(*pWrite), PROT_READ | PROT_WRITE, MAP_SHARED, fdWrite, 0);
+    close(fdWrite);
+    const char* ip = "localhost";
+    uint16_t port = IEC_60870_5_104_DEFAULT_PORT;
+
+    if (argc > 1)
+        ip = argv[1];
+
+    if (argc > 2)
+        port = atoi(argv[2]);
+
+    printf("Connecting to: %s:%i\n", ip, port);
+    CS104_Connection con = CS104_Connection_create(ip, port);
+
+    CS104_Connection_setConnectionHandler(con, connectionHandler, NULL);
+    CS104_Connection_setASDUReceivedHandler(con, asduReceivedHandler, NULL);
+
+    /* uncomment to log messages */
+    // CS104_Connection_setRawMessageHandler(con, rawMessageHandler, NULL);
+
+    if (CS104_Connection_connect(con)) {
+        printf("Connected!\n");
+
+        CS104_Connection_sendStartDT(con);
+
+        Thread_sleep(2000);
+
+        CS104_Connection_sendInterrogationCommand(con, CS101_COT_ACTIVATION, 1, IEC60870_QOI_STATION);
+
+        Thread_sleep(5000);
+
+#if 0
+        InformationObject sc = (InformationObject)
+                SingleCommand_create(NULL, 5000, true, false, 0);
+
+        printf("Send control command C_SC_NA_1\n");
+        CS104_Connection_sendProcessCommandEx(con, CS101_COT_ACTIVATION, 1, sc);
+
+        InformationObject_destroy(sc);
+
+        /* Send clock synchronization command */
+        struct sCP56Time2a newTime;
+
+        CP56Time2a_createFromMsTimestamp(&newTime, Hal_getTimeInMs());
+
+        printf("Send time sync command\n");
+        CS104_Connection_sendClockSyncCommand(con, 1, &newTime);
+#endif
+        printf("Wait ...\n");
+
+        Thread_sleep(1000);
+    }
+    else
+        printf("Connect failed!\n");
+
+    Thread_sleep(1000);
+
+    CS104_Connection_destroy(con);
+
+    printf("exit\n");
+}
+
